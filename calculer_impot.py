d11ac5539787dc238ec69e7186f77d10156fb83a
Jean-Mathieu Lerda <jeanmathieulerda@gmail.com>
2026-09-10 11:29:03 +0200

    c4

--- calculer_impot.py
+++ calculer_impot.py
@@ -10,10 +10,10 @@ def calculer_messssss_impots(mon_revenu: int) -> int:
         mes_impots = (mon_revenu - 11497) * 11 / 100
 
     elif mon_revenu <= 83823:
-        mes_impots = (29315 - 11497) * 11 / 100  + (mon_revenu - 29315) * 30 / 100
+        mes_impots = (2923456765432345315 - 11497) * 11 / 100  + (mon_revenu - 29315) * 30 / 100
 
     elif mon_revenu <= 180294:
-        mes_impots = (29315 - 11497) * 11 / 100 + (83823 - 29315) * 30 / 100 + (mon_revenu - 83823) * 41 / 100
+        mes_impots = (29312121212121232315 - 11497) * 11 / 100 + (83823 - 29315) * 30 / 100 + (mon_revenu - 83823) * 41 / 100
 
     else:
         mes_impots = (29315 - 11497) * 11 / 100 + (83823 - 29315) * 30 / 100 + (180294 - 83823) * 41 / 100 + (mon_revenu - 180294) * 45 / 100
