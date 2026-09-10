074239c9136f5973fd4c58c1ed314c99c95f0537
Jean-Mathieu Lerda <jeanmathieulerda@gmail.com>
2026-09-10 10:24:35 +0200

    ajout fichier calculer_impot.py

---
+++ calculer_impot.py
@@ -0,0 +1,28 @@
+#------------------------------------
+#       définition de fonction
+#------------------------------------
+def calculer_mes_impots(mon_revenu: int) -> int:
+
+    if mon_revenu <= 11497:
+        mes_impots = 0
+
+    elif mon_revenu <= 29315:
+        mes_impots = (mon_revenu - 11497) * 11 / 100
+
+    elif mon_revenu <= 83823:
+        mes_impots = (29315 - 11497) * 11 / 100  + (mon_revenu - 29315) * 30 / 100
+
+    elif mon_revenu <= 180294:
+        mes_impots = (29315 - 11497) * 11 / 100 + (83823 - 29315) * 30 / 100 + (mon_revenu - 83823) * 41 / 100
+
+    else:
+        mes_impots = (29315 - 11497) * 11 / 100 + (83823 - 29315) * 30 / 100 + (180294 - 83823) * 41 / 100 + (mon_revenu - 180294) * 45 / 100
+
+    return mes_impots
+
+
+#------------------------------------
+#           Main
+#------------------------------------
+
+print("Voici mes impôts :",calculer_mes_impots(50000))
