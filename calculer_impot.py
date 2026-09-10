06389da48ecc53a185b41cd0d9134f1957fecd94
Jean-Mathieu Lerda <jeanmathieulerda@gmail.com>
2026-09-10 10:52:43 +0200

    fix calculer_mes_impots

--- calculer_impot.py
+++ calculer_impot.py
@@ -3,7 +3,7 @@
 #------------------------------------
 def calculer_mes_impots(mon_revenu: int) -> int:
 
-    if mon_revenu <= 11450:
+    if mon_revenu <= 11500:
         mes_impots = 0
 
     elif mon_revenu <= 29315:
